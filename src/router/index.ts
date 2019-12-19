2f8cd7dd2d6a3a3ff8ac4bf1547f07cddade821f
mya-ake <make.app.ts@gmail.com>
2019-12-19 09:32:58 +0900

    chore: Add auto routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,20 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Router from 'vue-router';
+import routes from 'vue-auto-routing';
+import { createRouterLayout } from 'vue-router-layout';
 
-Vue.use(VueRouter);
+Vue.use(Router);
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-];
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes,
+const RouterLayout = createRouterLayout(layout => {
+  return import('@/layouts/' + layout + '.vue');
 });
 
-export default router;
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: RouterLayout,
+      children: routes,
+    },
+  ],
+});
